681fe031e33e420e196c8d04e1ad226f69b028e0
diego medeiros <diego.medeiros@geowellex.com>
2022-07-18 08:46:14 -0300

    model build and on operation

--- app/image_processing.py
+++ app/image_processing.py
@@ -3,8 +3,8 @@ import cv2 as cv
 from matplotlib import pyplot as plt
 
 
-def blur_image(image,mask_size=21):
-    """receive the initial image and apply the gaussian blur according to the 
+def blur_image(image, mask_size=21):
+    """receive the initial image and apply the gaussian blur according to the
     mask_size kernel passed as argument
 
     Args:
@@ -13,14 +13,15 @@ def blur_image(image,mask_size=21):
 
     Returns:
         _type_: blur image
-    """    
-    if mask_size%2 == 0:
-        mask_size = mask_size+1
-    blur_img = cv.GaussianBlur(image,(mask_size,mask_size),0)
+    """
+    if mask_size % 2 == 0:
+        mask_size = mask_size + 1
+    blur_img = cv.GaussianBlur(image, (mask_size, mask_size), 0)
     return blur_img
 
-def edges_canny(image,lower_threshold=50,multiplier=2,aperture_size=3):
-    """ apply the canny algorithm to evidence the image edges and draw 
+
+def edges_canny(image, lower_threshold=50, multiplier=2, aperture_size=3):
+    """apply the canny algorithm to evidence the image edges and draw
     it's contour
 
     Args:
@@ -30,42 +31,44 @@ def edges_canny(image,lower_threshold=50,multiplier=2,aperture_size=3):
         aperture_size (_type_): aperture size must be 3,5 ou 7
 
     Returns:
-        canny_img: img with the canny algorithm applyed with the parameters passed 
+        canny_img: img with the canny algorithm applyed with the parameters passed
         as argument
-    """    
+    """
     t_lower = lower_threshold  # Lower Threshold
-    t_upper = multiplier*lower_threshold  # Upper threshold
-    if aperture_size in [3,5,7]:
+    t_upper = multiplier * lower_threshold  # Upper threshold
+    if aperture_size in [3, 5, 7]:
         ap_size = aperture_size  # Aperture size
     else:
         ap_size = 3
-    
+
     # Applying the Canny Edge filter
-    canny_img = cv.Canny(image, t_lower, t_upper,apertureSize=ap_size)
+    canny_img = cv.Canny(image, t_lower, t_upper, apertureSize=ap_size)
     return canny_img
 
-def dilate_image(image,kernel_size=5,iterations=1):
-    """dilate the image according to the kernel size, this function is mainly used to 
-    dilate the canny's edges found on the edges_canny function to evidence it better to 
-    contours algorithm and bounding boxes. 
+
+def dilate_image(image, kernel_size=5, iterations=1):
+    """dilate the image according to the kernel size, this function is mainly used to
+    dilate the canny's edges found on the edges_canny function to evidence it better to
+    contours algorithm and bounding boxes.
 
     Args:
         image (_type_): numpy matrix with the image the user gonna dilate
-        kernel_size (int, optional): kernel's size of the boxe dilate kernel that the 
+        kernel_size (int, optional): kernel's size of the boxe dilate kernel that the
         function gonna apply on the image. Defaults to 5
         iterations(int): iterations of the dilate algorithm on the image.Defaults to 1.
 
     Returns:
         _type_: dilated image according to the kernel_size
-    """    
-    kernel = np.ones((kernel_size,kernel_size), np.uint8)
-    dilated_img = cv.dilate(image,kernel,iterations = iterations)
+    """
+    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    dilated_img = cv.dilate(image, kernel, iterations=iterations)
     return dilated_img
 
+
 def find_contours_and_boxes(image):
-    """find the contours of the image. This functions evidence the contours of the image, 
+    """find the contours of the image. This functions evidence the contours of the image,
     and intends to find the contours of the dilated image resulted from the dilate_image's
-    function. We evidence the contours to find the ROI(Region of interest) of the target 
+    function. We evidence the contours to find the ROI(Region of interest) of the target
     objects in the image and returns the bounding-boxes and the countors in two arrays.
 
     Args:
@@ -73,21 +76,24 @@ def find_contours_and_boxes(image):
 
     Returns:
         _type_: contours and the bounding boxes of the ROI's
-    """    
-    contours, hierarchy = cv.findContours(image, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    contours_poly = [None]*len(contours)
-    boundRect = [None]*len(contours)
-    centers = [None]*len(contours)
-    radius = [None]*len(contours)
+    """
+    contours, hierarchy = cv.findContours(
+        image, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE
+    )
+    contours_poly = [None] * len(contours)
+    boundRect = [None] * len(contours)
+    centers = [None] * len(contours)
+    radius = [None] * len(contours)
     for i, c in enumerate(contours):
         contours_poly[i] = cv.approxPolyDP(c, 3, True)
         boundRect[i] = cv.boundingRect(contours_poly[i])
-    
-    return contours_poly,boundRect,contours
 
-def draw_bounding_boxes_contours(gray_image,contours_poly,boundRect,contours):
-    """use the find_contours_and_boxes's function result to draw the countours 
-    into the target image. it intends to show how are the bounding boxes and the 
+    return contours_poly, boundRect, contours
+
+
+def draw_bounding_boxes_contours(gray_image, contours_poly, boundRect, contours):
+    """use the find_contours_and_boxes's function result to draw the countours
+    into the target image. it intends to show how are the bounding boxes and the
     contours
 
     Args:
@@ -98,11 +104,80 @@ def draw_bounding_boxes_contours(gray_image,contours_poly,boundRect,contours):
 
     Returns:
         _type_: _description_
-    """    
+    """
     gray_image2 = gray_image.copy()
     for i in range(len(contours)):
-        color = (0,0,0)#(rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
+        color = (
+            0,
+            0,
+            0,
+        )  # (rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
         cv.drawContours(gray_image2, contours_poly, i, color)
-        cv.rectangle(gray_image2, (int(boundRect[i][0]), int(boundRect[i][1])), \
-        (int(boundRect[i][0]+boundRect[i][2]), int(boundRect[i][1]+boundRect[i][3])), color, 2)
+        cv.rectangle(
+            gray_image2,
+            (int(boundRect[i][0]), int(boundRect[i][1])),
+            (
+                int(boundRect[i][0] + boundRect[i][2]),
+                int(boundRect[i][1] + boundRect[i][3]),
+            ),
+            color,
+            2,
+        )
     return gray_image2
+
+
+def extract_roi_by_boxes(image, boundRect):
+    """extract region of interest using the bounding boxes found in
+    find_contours_and_boxes.
+
+    Args:
+        image (_type_): numpy array with the original image
+        boundRect (_type_): Array with bounding boxes coordinates
+
+    Returns:
+        list: list of roi's extracted from the image
+    """
+    rois = []
+    for i in boundRect:
+        print(str(i[0]) + "\n")
+        rois.append(image[i[1] : (i[1] + i[3]), i[0] : i[0] + i[2]])
+
+    return rois
+
+
+def extract_roi_black_white(image, boundRect, threshold=170):
+    """extract roi with black and white
+
+    Args:
+        image (_type_): numpy array with the original image
+        boundRect (_type_): bound rects array with bounding boxes
+        threshold (_type_): lower threshold value
+
+    Returns:
+        list : array with roi's in black and white
+    """
+    letters_bw = []
+    letters = extract_roi_by_boxes(image, boundRect)
+    for letter in letters:
+        (thresh, blackAndWhiteLetter) = cv.threshold(
+            letter, threshold, 255, cv.THRESH_BINARY
+        )
+        letters_bw.append(255 - blackAndWhiteLetter)
+    return letters_bw
+
+
+def resize_rois(letters_bw):
+    """resize the images for a 28X28 image as the mnist source format
+
+    Args:
+        letters_bw (_type_): roi's array
+
+    Returns:
+        list: list with resized images
+    """
+    resized_letters = []
+    for letter_bw in letters_bw:
+        # resized_letter = 255 - resized_letter
+        resized_letter = cv.resize(letter_bw, (28, 28), interpolation=cv.INTER_AREA)
+        resized_letters.append(resized_letter)
+    return resized_letters
--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -2,4 +2,121 @@ import streamlit as st
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
+from image_processing import *
+import tensorflow as tf
 
+# import tensorflow as tf
+from tensorflow import keras
+
+img = cv.imread("digitos.jpg")
+
+img_gray = cv.cvtColor(img.copy(), cv.COLOR_BGR2GRAY)
+
+model = keras.models.load_model(
+    "/mnt/c/Users/diego/Desktop/projeto pdi_machine_learning/projeto-pdi_machine_learning/app/model"
+)
+st.session_state.original_image = img_gray
+
+st.session_state.moment_image = img_gray.copy()
+st.session_state.stage = "boxes_contours"
+if st.session_state.stage == "initial":
+    img = cv.imread("digitos.jpg")
+    st.session_state.original_image = img
+    img_gray = cv.cvtColor(img.copy(), cv.COLOR_BGR2GRAY)
+    st.session_state.moment_image = img_gray.copy()
+    st.image(img_gray)
+
+if st.session_state.stage == "blur":
+    mask_size = st.slider(label="Gaussian mask size", min_value=5, max_value=71)
+    st.session_state.moment_image = blur_image(
+        st.session_state.moment_image, mask_size=mask_size
+    )
+    st.image(st.session_state.moment_image)
+if st.session_state.stage == "canny":
+    multiplier = st.slider(
+        label="Canny multiplier for threshold", min_value=2, max_value=7
+    )
+    st.session_state.moment_image = edges_canny(
+        st.session_state.moment_image,
+        lower_threshold=100,
+        multiplier=multiplier,
+        aperture_size=3,
+    )
+    st.image(st.session_state.moment_image)
+
+if st.session_state.stage == "dilate":
+    kernel_size = st.slider(label="Kernel size for dilate", min_value=5, max_value=8)
+    interation = st.slider(
+        label="max iterations for dilate function", min_value=1, max_value=8
+    )
+    st.session_state.moment_image = dilate_image(
+        st.session_state.moment_image, kernel_size=kernel_size, iterations=interation
+    )
+    st.image(st.session_state.moment_image)
+
+if st.session_state.stage == "boxes_contours":
+    mask_size = st.slider(label="Gaussian mask size", min_value=5, max_value=71)
+    st.session_state.moment_image = blur_image(
+        st.session_state.moment_image, mask_size=mask_size
+    )
+    multiplier = st.slider(
+        label="Canny multiplier for threshold", min_value=2, max_value=7
+    )
+    aperture = st.slider(label="Canny aperture size", min_value=3, max_value=7, step=2)
+    st.session_state.moment_image = edges_canny(
+        st.session_state.moment_image,
+        lower_threshold=100,
+        multiplier=multiplier,
+        aperture_size=aperture,
+    )
+    kernel_size = st.slider(label="Kernel size for dilate", min_value=5, max_value=8)
+    interation = st.slider(
+        label="max iterations for dilate function", min_value=1, max_value=5
+    )
+    st.session_state.moment_image = dilate_image(
+        st.session_state.moment_image, kernel_size=kernel_size, iterations=interation
+    )
+    contours_poly, boundRect, contours = find_contours_and_boxes(
+        st.session_state.moment_image
+    )
+    st.session_state.bound_rect = boundRect
+    boxes_img = draw_bounding_boxes_contours(
+        st.session_state.original_image.copy(), contours_poly, boundRect, contours
+    )
+    st.image(boxes_img)
+    button = st.button(label="extract roi")
+    button_predict = st.button(label="predict")
+    if button:
+        rois = extract_roi_black_white(
+            st.session_state.original_image.copy(), st.session_state.bound_rect
+        )
+
+        resize = resize_rois(rois)
+        for roi in resize:
+            st.image(roi)
+    if button_predict:
+        rois = extract_roi_black_white(
+            st.session_state.original_image.copy(), st.session_state.bound_rect
+        )
+        resize = resize_rois(rois)
+        #        try:
+        resize2 = list(np.float_(resize) / 255)
+        print(resize2.__format__)
+        imgs = np.array([np.reshape(np.array(i), (28, 28)) for i in resize2])
+        predctions = model.predict(imgs)
+        for i in range(len(resize)):
+            st.image(resize[i])
+            st.write(np.argmax(predctions[i]))
+        ## except:
+        ##     st.warning(
+        ##         "Alguma coisa deu errado, lembrando que você deve enviar no mínimo duas imagens"
+        ##     )
+
+# if st.session_state.stage == "predict":
+#    st.session_state.rois = rois
+#     for roi in rois:
+#         st.image(roi)
+#     button_r = st.button(label="resize")
+#     if button_r:
+#         st.session_state.stage = "resized"
+# if st.session_state.stage == "resized":
